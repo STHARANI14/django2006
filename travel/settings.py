a16982184e4b312c99e26829d69b13ac362682b5
STHARANI14 <143706011+STHARANI14@users.noreply.github.com>
2024-03-03 19:59:38 +0530

    Update settings.py

--- travel/settings.py
+++ travel/settings.py
@@ -74,24 +74,14 @@ WSGI_APPLICATION = 'travel.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TRAVEL2',
-        'USER':'postgres',
-        'PASSWORD':'Ssvmanoj9',
-        'HOST':'localhost',
-        'PORT':'5432'
-    }
-}
+
 
 
 # Password validation
